92cf67956da457e99c12ca3ac40aa8c1d96b95d6
diegomated1 <diegocardwr@gmail.com>
2023-03-12 13:07:26 -0500

    using socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,14 @@
-// IMPORTS
-import dotenv from 'dotenv';
-import express from 'express';
-import morgan from 'morgan';
-const app = express();
+import { Server } from "socket.io";
 
-// CONFIG
-dotenv.config();
+const io = new Server(3000, {
+    cors: {
+      origin: '*'
+    }
+});
 
-// MIDDLEWARES
-app.use(express.json());
-app.use(morgan('dev'));
+io.on("connection", (socket) => {
 
-// ROUTER
-
-// LISTEN
-app.listen(parseInt(process.env.API_PORT!), process.env.API_HOST!, ()=>{
-    console.log(`Listen on http://${process.env.API_HOST}:${process.env.API_PORT}/`);
+    socket.on("disconnect", ()=>{
+      console.log("disconnected");
+    });
 });
