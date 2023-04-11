d131510b519ff268f7ff622105a7fc593fa0dcc2
diegomated1 <diegocardwr@gmail.com>
2023-04-11 15:34:14 -0500

    change structure

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,51 @@
-// Importar el módulo de Socket.IO
-import { Server } from "socket.io";
+import dotenv from 'dotenv';
+import express, { Application } from 'express';
 import http from 'http';
-import chatController from "./controllers/message.controller.js";
+import io from 'socket.io';
+import ChatListeners from "./listeners/chat.listeners.js";
+import cors from 'cors';
 
-// Crear una instancia de Socket.IO
-const server = http.createServer();
-server.listen(3005, '192.168.1.22');
+class App{
 
-const io = new Server(server, {
-  cors: {
-    origin: "*",
+  app: Application
+  server: http.Server
+  io: io.Server
+
+  constructor(){
+      this.app = express();
+      this.server = new http.Server(this.app);
+      this.io = new io.Server(this.server, {
+          cors: {
+              origin: process.env.CLIENT_HOST || true
+          }
+      });
+      this.config();
+      this.routes();
+      this.start();
   }
-});
 
-// Escuchar eventos de conexión en el servidor
-io.on("connection", (socket) => {
-  chatController(io, socket);
+  config(){
+      dotenv.config();
+      this.app.use(cors({
+          origin: "*"
+      }));
+      this.app.use(express.json());
+  }
 
-  // Escuchar el evento de desconexión
-  socket.on("disconnect", () => {
-    console.log("Usuario desconectado");
-  });
-});
+  routes(){
+  }
+
+  start(){
+    this.io.on('connection', (socket)=>{
+      const {id_chat} = socket.handshake.query;
+      if(id_chat){
+        socket.join(`chat:${id_chat}`);
+      }
+      new ChatListeners(this.io, socket);
+    });
+
+    this.server.listen(parseInt(process.env.SERVER_HOST||'3000'), '10.153.50.142');
+  }
+}
+
+const app = new App();
